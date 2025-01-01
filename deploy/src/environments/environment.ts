1f3d6cef0486ea9907cb9085cc3579b3e1d9f80b
Charly <charly.flu.67@gmail.com>
2025-01-01 15:48:17 +0100

    Trying yet somthing else

---
+++ deploy/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  backendProduct: 'https://cnamapp-xzd8.onrender.com/api/products',
+  backendRegister: 'https://cnamapp-xzd8.onrender.com/api/users/register',
+  backendLogin: 'https://cnamapp-xzd8.onrender.com/api/users/login',
+  backendUser: 'https://cnamapp-xzd8.onrender.com/api/users/user',
+};

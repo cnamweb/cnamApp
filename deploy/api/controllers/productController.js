530afee488bc327401b127a6b45fbabda3e90208
Charly <charly.flu.67@gmail.com>
2025-01-19 18:58:30 +0100

    Changed to deploy project

--- deploy/api/controllers/productController.js
+++ deploy/api/controllers/productController.js
@@ -1,6 +1,41 @@
-const products = require('../models/products');
+const db = require("../models");
+const Products = db.products;
+const Op = db.Sequelize.Op;
 
 exports.get = (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(products));
+    Products.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving products."
+            });
+        });
+    
+}
+
+exports.getFiltered = (req, res) => {
+    const { category, name } = req.body;
+    Products.findAll({ where: { category:  { [Op.like]: `%${category}%` }, namepr: { [Op.like]: `%${name}%` } } })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || `Some error occurred while retrieving products with category ${category} and name like ${name}.`
+            });
+        });
+}
+
+exports.getAllCategories = (req, res) => {
+    Products.findAll({ attributes: ['category'] })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving categories."
+            });
+        });
 }
